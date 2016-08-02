c2b2ceac58779759cd51161442b53e338edb5d51
Kylie Soderblom <MacDream@Kylies-MacBook-Pro.local>
2016-08-02 15:34:40 -0700

    functions in place, syntax correction needed, combine timer 10sec left with run function(?), pull in function resultPage(){ when renderQ and checkA are functioning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,15 +3,18 @@ var correct = 0;
 var incorrect = 0;
 var noAnswer = 0;
 var score = 0;
-var test, test_status, question, choice, choices, chA, chB, chC,chD; 
+var pos = 0
+var test, test_status, question, choice, chA, chB, chC,chD; 
 var seconds, temp;
 var number = 120;
+var choices = [];
 
 var questions = [
 	["According to the 'Huffington Post', what can I guy do without?", "Black Umbrella", "Desert Boots","Bermuda shorts", "Converse sneakers","C"],
 	["According to the 'Huffington Post', what can I guy do without?","Navy Suit", "Gray Sport Coat","Dark Wool Jacket","Trench Coat","D"],
 	["According to GQ, what can I guy do without?","Black Dress Pants","Oxford Shirt","Bomber Jacket","White Sneakers","A"],
-	["According to 'The Elite Daily', of the traits that women want in a man, what is not included?", "Sense of Humor", "Brains", "Sex Appeal", "Good Listener","C"]
+	["According to 'The Elite Daily', of the traits that women want in a man, what is not included?", "Sense of Humor", "Brains", "Sex Appeal", "Good Listener","C"],
+	["What was the first year Corvette introduced the Sting Ray?", "1969", "1961","1965","1963","D"]
 	];
 
 $(document).ready(function(){
@@ -24,14 +27,16 @@ $(document).ready(function(){
 	run(); // countdown timer
 	//$("#Start").show("slow"); idea is idStart hides & .showTimer takes its space
 	$(".showTimer").html('number');
+	renderQuestion();
 
 
 	});
 
-	function Reset() {
+	function reset() {
 		correct = 0;
 		incorrect = 0;
 		noAnswer = 0;
+		pos = 0;
 		number = 120;
 		$("#Start").show();// if using same button for :timer & done, ???
 		$("#Start").html("Start");
@@ -49,14 +54,14 @@ function tenleftSeconds(){
 }
 
 function totalSeconds() {
-	audio.play();
+	//audio.play();
 	
 	alert("Time is Up");
 	console.log('done');
 }
 
 	//time remaining
-	function run(){
+	function run(){  //run(); - this will happen on #Start.on (click, run check syntax)
             counter = setInterval(decrement, 1000);
         }
 
@@ -75,31 +80,57 @@ function totalSeconds() {
             reset();
         }
 
-        //run(); - this will happen on #Start.on (click, run check syntax)
-
-
-});
-	/*Question section
-	function (x){
-		return document.getElementById('x');
-	}
-	// following only half relevant if doing Easy hw
-	function renderQuestion() {
-		test = ('test');
-		// question 1 of 4
-		x$("test_status").html = question "+ (pos+1)+" + "of" + questions.length; }
-	
+        //Populating question form
+    //function
+   	function renderQuestion() {
+   		if(pos >= questions.length){
+   			$("#testStatus").html = "<h2> You got '+correct+' of '+questions.length+'  questions correct </h2>";
+   			$("#testStatus").html = "Test Completed";
+   			reset();
+   			return false;
+   		}
+		//test = ('test'); //populate questions 1 thru 5
+		$("#testStatus").html = ("Question" + (pos+1) +"  of  " + questions.length);
 		question = questions [pos] [0]; // actual question
 		chA = questions [pos] [1];			// choice one etc.
-		chB = questions [pos] [2];
+		chB = questions [pos] [2]; 
 		chC = questions [pos] [3];
 		chD = questions [pos] [4];
-		$("question").html = <h2>"+question +"<h2>;
-		$("textInput").html += <input type = 'radio' name = 'choices' //+= append?
-		/* need help here - 
-		value"A">"+chA"<br>...
-		value"B">
-		value"C">
+		$("#textInput").html = "<h2>'+question +'<h2>";
+		$("#textInput").html += "<input type ='radio' name = 'choices'  value = 'A'> '+chA+' <br>";
+		$("#textInput").html += "<input type = 'radio' name = 'choices' value = 'B'> '+chB+' <br>";
+		$("#textInput").html += "<input type = 'radio' name = 'choices' value = 'C'> '+chC+' <br>";
+		$("#textInput").html += "<input type = 'radio' name = 'choices' value = 'D'> '+chD+' <br><br>";
+		$("#textInput").html += "<button onclick="checkAnswer()">Submit Answer</button>";
+		}
+		
+	function checkAnswer(){
+		choices = document.getElementByName('choices'); //creating array with 4 items
+		for(var i=0; i<choices.length; i++){
+			if(choices [i].checked){
+				choice = choices[i].value;		
+			}
+		}
+		if (choice === questions [pos] [5]){
+			correct ++; // could add sound audio() or image flash
+			}
+		else if {
+			(choice !== questions [pos] [5])
+				incorrect ++
+			}
+		else {
+			unanswered ++
+			}
+		}
+			pos ++;
+			renderQuestion();
+   
+}); //end of document.ready
+
+	/*Question section
+	
+	// following only half relevant if doing Easy hw
+
 	function checkAnswer() {
 		choices = document.getElementByName('choices');
 		for(var i=0; i<choices.length; i++){
@@ -127,4 +158,16 @@ function totalSeconds() {
 			$("textInput").html("<h3> "Incorrect Answers: "+ (incorrect)</h3><br>" );
 			$("textInput").html("<h3> "Unanswered: "+ (noAnswer)</h3><br>" );*/
 
+// lager, ale, pilsner, bock
+// sports
+//cars engines
+//what was the first yer the corvette became a stingray
+// 1969 Indianopis pace car - mustanf, camaro, corvette baracuda
+// what are the only twpo days that there are no professional spors being played - day before and day after allstar game
+//what is commonly known as a gordy howe hat trick - goal, assist fight misconduct penalty
+// price is right fail tune 
+// last days of our lives
+// in golf which of these clubs has the least loft: pitching wedge, sand wedge, lob wedge and gap wedge, answer pitching
+//  Which film brought a 1981 Oscar for Robert De Niro?
 
+//A Raging Bull B Taxi Driver C The Deer Hunter D The King of Comedy
